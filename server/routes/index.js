41e2b8f54914297fa9d64a6307de108457e8d62b
haoxiangyin <haoxiangyin@hotmail.com>
2022-08-24 17:33:51 +0800

    Fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,15 @@ var minZoom = tileInfo.minZoom
 var maxZoom = tileInfo.maxZoom
 
 for (var i = minZoom; i <= maxZoom; i++) {
-  eval(`var file${i} = '../data/${i}.sqlite'`)
-  eval(`var db${i} = new sqlite3.Database(file${i})`)
+  eval(`var file${i}f1 = '../data/${i}f1.sqlite'`)
+  eval(`var file${i}f2 = '../data/${i}f2.sqlite'`)
+  eval(`var file${i}f3 = '../data/${i}f3.sqlite'`)
+  eval(`var file${i}f4 = '../data/${i}f4.sqlite'`)
+  eval(`var db${i}f1 = new sqlite3.Database(file${i}f1)`)
+  eval(`var db${i}f2 = new sqlite3.Database(file${i}f2)`)
+  eval(`var db${i}f3 = new sqlite3.Database(file${i}f3)`)
+  eval(`var db${i}f4 = new sqlite3.Database(file${i}f4)`)
+
 }
 // 查询pbf数据接口
 router.get('/getData', (req, res) => {
@@ -36,7 +43,8 @@ router.get('/getPngData', (req, res) => {
   let x = req.query.x
   let y = req.query.y
   let z = req.query.z
-  let db = eval(`db${z}`)
+  let f = req.query.f;
+  let db = eval(`db${z}${f}`)
   db.all(`SELECT * FROM tiles WHERE (z,x,y)=(${z},${x},${y})`, function (err, row) {
     if (row && row[0]) {
       res.write(row[0].tile_data)
